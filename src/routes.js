2fdd6107d9fa831de253cf9e61aa2a50181f8712
alamgirjuab <alamgirjuab@gmail.com>
2022-01-22 22:29:51 +0600

    title updated

--- src/routes.js
+++ src/routes.js
@@ -15,8 +15,8 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-/* import Index from "views/Index.js";
-import Profile from "views/examples/Profile.js";
+import Index from "views/Index.js";
+/*import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
 import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js"; */
@@ -24,34 +24,34 @@ import Tables from "views/examples/Tables.js";
 // import Icons from "views/examples/Icons.js";
 
 var routes = [
-  /* {
+  {
     path: "/index",
     name: "Dashboard",
     icon: "ni ni-tv-2 text-primary",
     component: Index,
     layout: "/admin",
   },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: Maps,
-    layout: "/admin",
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
-    component: Profile,
-    layout: "/admin",
-  }, */
+  /* {
+     path: "/icons",
+     name: "Icons",
+     icon: "ni ni-planet text-blue",
+     component: Icons,
+     layout: "/admin",
+   },
+   {
+     path: "/maps",
+     name: "Maps",
+     icon: "ni ni-pin-3 text-orange",
+     component: Maps,
+     layout: "/admin",
+   },
+   {
+     path: "/user-profile",
+     name: "User Profile",
+     icon: "ni ni-single-02 text-yellow",
+     component: Profile,
+     layout: "/admin",
+   }, */
   {
     path: "/tables",
     name: "Tables",
